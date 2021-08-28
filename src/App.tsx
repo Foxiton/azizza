e4fcfceb8ccc02df17cd1eef4f97562508c1e1cb
Amel Salvin <amalsalvin97@gmail.com>
2021-08-28 05:34:35 +0530

    sidebar integrated to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,36 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.scss';
+import Sidebar from './components/Sidebar/Sidebar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import styled from 'styled-components';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Title>Hello World!</Title>
-      </header>
+      <Router>
+        <Sidebar/>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          <Title>Hello World!</Title>
+        </header>
+        <Switch>
+          <Route path='/' exact />
+          <Route path='/reports' />
+          <Route path='/products' />
+        </Switch>
+      </Router>
     </div>
   );
 }
