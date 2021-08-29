a5b32be5ef02b822701398b65dc31272446574b6
Amel Salvin <amalsalvin97@gmail.com>
2021-08-29 15:17:58 +0530

    global variable added to stories and app

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,6 +2,8 @@ import React from "react";
 import { addDecorator } from "@storybook/react";
 import { MemoryRouter } from "react-router";
 
+import '../src/styles/variables.scss';
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
