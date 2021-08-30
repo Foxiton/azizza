21df12228c15bd560b6e52914faba46150a96be7
Amel Salvin <amalsalvin97@gmail.com>
2021-08-30 23:36:42 +0530

    code refactor: App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.scss';
-import Sidebar from './components/Sidebar/Sidebar';
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
-import styled from 'styled-components';
+
+import Sidebar from './components/Sidebar/Sidebar';
+import Dashboard  from "./screens/Dashboard/Dashboard";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Sidebar/>
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          <Title>Hello World!</Title>
-        </header>
         <Switch>
           <Redirect exact from="/" to="/dashboard" />
-          <Route path='/' />
+          <Route path='/dashboard' component={Dashboard}/>
           <Route path='/reports' />
-          <Route path='/products' />
+          <Route path='/messages' />
+          <Route path='/settings' />
         </Switch>
       </Router>
     </div>
   );
 }
-
-
-const Title = styled.h1`
-  font-size: 1.5em;
-  text-align: center;
-  color: palevioletred;
-`;
 
 export default App;
