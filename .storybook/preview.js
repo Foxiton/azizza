ac8c1c59a88171f18cb99a13d9be30a975cdb7f0
Amel Salvin <amalsalvin97@gmail.com>
2021-09-02 15:59:33 +0530

    storybook added index stylesheet

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -4,6 +4,7 @@ import { MemoryRouter } from "react-router";
 
 import '../src/styles/variables.scss';
 import '../src/styles/fonts.scss';
+import '../src/styles/index.scss';
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
