fa4afc997eb19f10d1462861d7db87f45d8721b6
Amel Salvin <amalsalvin97@gmail.com>
2021-09-15 02:04:40 +0530

    header and first scroll row implemented

--- src/screens/Dashboard/Dashboard.tsx
+++ src/screens/Dashboard/Dashboard.tsx
@@ -1,15 +1,45 @@
 import React from 'react';
 import logo from '../../assets/logo.svg';
-
+import * as IoIcons from "react-icons/io";
+import * as FiIcons from "react-icons/fi";
 import styled from 'styled-components';
 
+import Card, { LargeCard } from '../../components/Card/Card';
 import './Dashboard.scss';
+
+import imageOnTop from '../../assets/onTop.jpg'
 
 function App() {
   return (
       <div className="app-main-content-wrapper scrollable">
         <div className="main-app-content-container">
-          <img src={logo} className="App-logo" alt="logo" />
+          <header>
+            <h1>Hello, Jeremy! 👋</h1>
+            <div>
+              <FiIcons.FiSearch className="search-icon" size="20px" color="darkgrey"/>
+              <input placeholder="Search Destination..." type="text" />
+            </div>
+            <button className="notification-icon-wrapper badge">
+              <IoIcons.IoIosNotificationsOutline />
+            </button>
+          </header>
+          <p className="welcome-text">Welcome back and explore the world</p>
+          {/* <div className="grid-test" contentEditable>
+            aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+          </div> */}
+          <div className="destination-suggestion-container scrollable">
+            <LargeCard background={imageOnTop} title="Test 123"/>
+            <LargeCard background={imageOnTop} title="Test 123"/>
+            <LargeCard background={imageOnTop} title="Test 123"/>
+            <LargeCard background={imageOnTop} title="Test 123"/>
+            <LargeCard background={imageOnTop} title="Test 123"/>
+            <LargeCard background={imageOnTop} title="Test 123"/>
+            <LargeCard background={imageOnTop} title="Test 123"/>
+            <LargeCard background={imageOnTop} title="Test 123"/>
+            <LargeCard background={imageOnTop} title="Test 123"/>
+            <LargeCard background={imageOnTop} title="Test 123"/>
+          </div>
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
           <p>
           Edit <code>src/App.tsx</code> and save to reload.
           </p>
