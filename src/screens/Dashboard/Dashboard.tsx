d24197dc9bb097b351eb4bfeeb1eda8e8dd0a2fb
Amel Salvin <amalsalvin97@gmail.com>
2021-09-16 20:21:32 +0530

    added debounce package | window resize scroll width

--- src/screens/Dashboard/Dashboard.tsx
+++ src/screens/Dashboard/Dashboard.tsx
@@ -1,18 +1,37 @@
-import React from 'react';
-import logo from '../../assets/logo.svg';
+import React, {useRef, useEffect, useState} from 'react';
+import styled from 'styled-components';
+import { useDebouncedCallback } from 'use-debounce';
+
 import * as IoIcons from "react-icons/io";
 import * as FiIcons from "react-icons/fi";
-import styled from 'styled-components';
 
-import Card, { LargeCard } from '../../components/Card/Card';
+import { destinationSuggestions } from '../../globals/constants/dashboardData';
+import { LargeCard } from '../../components/Card/Card';
+import imageOnTop from '../../assets/onTop.jpg'
 import './Dashboard.scss';
 
-import imageOnTop from '../../assets/onTop.jpg'
 
 function App() {
+  const appContainerRef: React.RefObject<HTMLDivElement> = useRef(null);
+
+  const [appContainerWidth, setAppContainerWidth] = useState<number | undefined>(0);
+
+  const resizeHandler = useDebouncedCallback(() => {
+    setAppContainerWidth(0);
+    setAppContainerWidth(appContainerRef.current?.clientWidth)          
+  }, 200);
+
+  useEffect(() => {
+    setAppContainerWidth(appContainerRef.current?.clientWidth);
+    window.addEventListener('resize', resizeHandler)
+    return (() => {
+      window.removeEventListener('resize', resizeHandler);
+    })
+  }, [resizeHandler]);
+
   return (
       <div className="app-main-content-wrapper scrollable">
-        <div className="main-app-content-container">
+        <div className="main-app-content-container" ref={appContainerRef}>
           <header>
             <h1>Hello, Jeremy! 👋</h1>
             <div>
@@ -24,52 +43,31 @@ function App() {
             </button>
           </header>
           <p className="welcome-text">Welcome back and explore the world</p>
-          {/* <div className="grid-test" contentEditable>
-            aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-          </div> */}
-          <div className="destination-suggestion-container scrollable">
-            <LargeCard background={imageOnTop} title="Test 123"/>
-            <LargeCard background={imageOnTop} title="Test 123"/>
-            <LargeCard background={imageOnTop} title="Test 123"/>
-            <LargeCard background={imageOnTop} title="Test 123"/>
-            <LargeCard background={imageOnTop} title="Test 123"/>
-            <LargeCard background={imageOnTop} title="Test 123"/>
-            <LargeCard background={imageOnTop} title="Test 123"/>
-            <LargeCard background={imageOnTop} title="Test 123"/>
-            <LargeCard background={imageOnTop} title="Test 123"/>
-            <LargeCard background={imageOnTop} title="Test 123"/>
-          </div>
-          {/* <img src={logo} className="App-logo" alt="logo" /> */}
-          <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-          >
-          Learn React
-          </a>
-          <Title>Hello World!</Title>
-          <Title>Hello World!</Title>
-          <Title>Hello World!</Title>
-          <Title>Hello World!</Title>
-          <Title>Hello World!</Title>
-          <Title>Hello World!</Title>
-          <Title>Hello World!</Title>
-          <Title>Hello World!</Title>
-          <Title>Hello World!</Title>
-          <Title>Hello World!</Title>
+          <DestSuggestionWrapper
+            className="destination-suggestion-container scrollable"
+            width={appContainerWidth}
+            >
+              {
+                destinationSuggestions.map((item, index) => (
+                  <LargeCard key={index} background={imageOnTop} title={item.title} place={item.place}/>                  
+                ))
+              }
+          </DestSuggestionWrapper>
         </div>
       </div>
   );
 }
 
-const Title = styled.h1`
-  font-size: 1.5em;
-  text-align: center;
-  color: palevioletred;
+const DestSuggestionWrapper = styled.div.attrs((props: any) => ({
+  width: props.width
+}))`
+  width: ${props => props.width}px;
 `;
+
+// const Title = styled.h1`
+//   font-size: 1.5em;
+//   text-align: center;
+//   color: palevioletred;
+// `;
 
 export default App;
