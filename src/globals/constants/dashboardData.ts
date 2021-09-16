af159d6be5d1d04fbd290756598efa9ab495c7ae
Amel Salvin <amalsalvin97@gmail.com>
2021-09-16 20:19:37 +0530

    created dashboard data

---
+++ src/globals/constants/dashboardData.ts
@@ -0,0 +1,52 @@
+export const destinationSuggestions = [
+    {
+        title: "Mount Forel",
+        place: "Greenland"
+    },
+    {
+        title: "Eco Camping",
+        place: "Patagonia"
+    },
+    {
+        title: "Mount Everest",
+        place: "Nepal"
+    },
+    {
+        title: "Gioc Waterfall",
+        place: "Iceland"
+    },
+    {
+        title: "Tartaruga Camp",
+        place: "Hong"
+    },
+    {
+        title:"Tria Camp",
+        place:"Ireland"
+    },
+    {
+        title:"Tria Camp",
+        place:"Ireland"
+    },
+    {
+        title:"Crooked Valley",
+        place:"CrooksLand"
+    },
+    {
+        title:"Niagara Falls",
+        place:"Ontario"
+    },
+    {
+        title:"Nandhi Trek",
+        place:"Bangalore"
+    }
+  ];
+
+
+  
+
+
+
+ 
+
+
+
