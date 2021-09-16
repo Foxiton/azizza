d223b04f6c8ec9046405fa41e6471b73c653e90f
Amel Salvin <amalsalvin97@gmail.com>
2021-09-16 20:18:57 +0530

    clean code

--- src/components/RightSidebar/RightSidebar.tsx
+++ src/components/RightSidebar/RightSidebar.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 
-import { Link } from 'react-router-dom';
-
 import { ScheduleData } from '../../globals/constants/sidebarData';
 
 import UserCard from "../UserCard/UserCard";
